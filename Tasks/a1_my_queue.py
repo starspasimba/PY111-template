238127e86c0b4a6357f10e047db8c55285c23b19
user <ilnurf@gmail.com>
2019-12-11 21:08:11 +0300

    try a1

--- Tasks/a1_my_queue.py
+++ Tasks/a1_my_queue.py
@@ -3,7 +3,7 @@ My little Queue
 """
 from typing import Any
 
-
+que: list = []
 def enqueue(elem: Any) -> None:
 	"""
 	Operation that add element to the end of the queue
@@ -11,7 +11,8 @@ def enqueue(elem: Any) -> None:
 	:param elem: element to be added
 	:return: Nothing
 	"""
-	print(elem)
+	global que
+	que.append(elem)
 	return None
 
 
@@ -21,7 +22,14 @@ def dequeue() -> Any:
 
 	:return: dequeued element
 	"""
-	return None
+	return_elem = 0
+	global que
+	if que:
+		return_elem = que[0]
+		del que[0]
+	else:
+		return_elem = None
+	return return_elem
 
 
 def peek(ind: int = 0) -> Any:
@@ -31,8 +39,14 @@ def peek(ind: int = 0) -> Any:
 	:param ind: index of element (count from the beginning)
 	:return: peeked element
 	"""
-	print(ind)
-	return None
+	peek_elem = 0
+	global que
+	if que:
+		peek_elem = que[ind]
+	else:
+		peek_elem = None
+
+	return peek_elem
 
 
 def clear() -> None:
@@ -41,4 +55,7 @@ def clear() -> None:
 
 	:return: None
 	"""
+	global que
+	que = []
 	return None
+
