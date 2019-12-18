26a150bed08e853cba75a1e87622d2e69875d0e7
user <ilnurf@gmail.com>
2019-12-18 16:28:02 +0300

    try a3

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,14 @@ def check_brackets(brackets_row: str) -> bool:
 	:param brackets_row: input string to be checked
 	:return: True if valid, False otherwise
 	"""
-
+	if brackets_row[0] == ")":
+		return False
+	x = 0
+	for i in brackets_row:
+		if i == "(":
+			x += 1
+		else:
+			x -= 1
+	if x == 0:
+		return True
 	return False
