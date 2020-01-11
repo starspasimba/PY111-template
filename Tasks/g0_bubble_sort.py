2eeb75ff6cbf3e6fa2f4c25d44942955b923c47f
user <ilnurf@gmail.com>
2020-01-11 14:50:02 +0300

    try g0

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,4 +10,12 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
+	for i in range(len(container), 0, -1):
+		for x in range(i):
+			if ((x + 1) < i) and (container[x]>container[x + 1]):
+				z = container[x + 1]
+				container[x + 1] = container[x]
+				container[x] = z
+
 	return container
+ 
